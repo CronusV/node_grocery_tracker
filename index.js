e311c089866f32fe3246fd032bd0db9b4d97b56f
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-13 13:26:31 -0700

    Refactored main function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let groceryList = {};
+// TODO Sanitation, duplicates, and errors.
+let groceryList = [];
 
+const { stdin } = require('process');
 // Import the readline module for handling user input in the console
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -7,37 +9,41 @@ const rl = readline.createInterface({
   output: process.stdout, // Write to standard output (console)
 });
 
-// rl.on('line', (line) => {
-//   console.log(line);
-//   console.log('test');
-// });
-
 rl.once('close', () => {
   // end of input
 });
 
-// rl.on('pause', () => {
-//   console.log('This is paused');
-// });
-
-function main() {
-  let userInput = 'Yes';
-
-  console.log('would you like to add to grocery list? Type Yes or No');
-  rl.on('line', (stdin) => {
-    if (stdin === 'No') {
-      rl.close();
-    } else if (stdin === 'Yes') {
-      // Go to another function
-
-      // Here should ask if they want to add to grocery list
-      console.log('would you like to add to grocery list? Type Yes or No');
-    } else {
-      console.log('Invalid input, please try again');
-      console.log('would you like to add to grocery list? Type Yes or No');
-    }
-  });
+function getItemFromUser() {
+  return { item, quantity, price, bought };
 }
 
+async function main() {
+  rl.question(
+    `Here are the options
+  1. Add 
+  2. Delete
+  3. Set Item to bought
+  4. Display Grocery List
+  5. Exit
+  `,
+    (res) => {
+      switch (res) {
+        case '1':
+          break;
+        case '2':
+          break;
+        case '3':
+          break;
+        case '4':
+          break;
+        case '5':
+          rl.close();
+      }
+    }
+  );
+
+  rl.close();
+}
+
+console.log('Welcome to grocery shopping tracker!');
 main();
-rl.close();
